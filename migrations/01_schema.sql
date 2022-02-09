8451d0d218e29248477f3921dfcc15f3c4d6a7d6
Vivian Zhang <vvynz12@gmail.com>
2022-02-09 20:48:13 +0000

    had to drop and re-add a table due to a spelling error

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -21,7 +21,7 @@ CREATE TABLE properties (
   number_of_bedrooms INTEGER NOT NULL DEFAULT 0,
 
   country VARCHAR(255) NOT NULL,
-  streeet VARCHAR(255) NOT NULL,
+  street VARCHAR(255) NOT NULL,
   city VARCHAR(255) NOT NULL,
   province VARCHAR(255) NOT NULL,
   post_code VARCHAR(255) NOT NULL,
