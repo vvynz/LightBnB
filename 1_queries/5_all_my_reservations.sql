32d224b180a5ef4afb58ac6853f5f385c0b0ad05
Vivian Zhang <vvynz12@gmail.com>
2022-02-09 22:26:38 +0000

    practicing select queries using the databases set up for LightBnB

---
+++ 1_queries/5_all_my_reservations.sql
@@ -0,0 +1,8 @@
+SELECT properties.*, reservations.*, avg(property_reviews.rating) AS avg_rating
+FROM reservations
+JOIN properties ON reservations.property_id = properties.id
+JOIN property_reviews ON properties.id = property_reviews.property_id
+WHERE reservations.guest_id = 1 AND reservations.end_date < now()::date
+GROUP BY properties.id, reservations.id
+ORDER BY start_date
+LIMIT 10;
